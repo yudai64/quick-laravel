7b1ea097a222f856d9b5f2c668ed15fb247840c4
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-22 08:56:20 +0900

    共通レイアウトの作成

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+<body>
+<img src="https://wings.msn.to/image/wings.jpg" title="ロゴ"/>
+<hr />
+@section('main')
+<p>既存のコンテンツです</p>
+@show
+<hr />
+<p>Copyright(c) 1998-2019, WINGS Project. All Right Reserved.</p>
+</body>
+</html>
